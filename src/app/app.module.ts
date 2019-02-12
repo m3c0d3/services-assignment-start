7b2aab033d2c8eeeac968fb3ece0a0ff5c0a7e9e
marius.antal <marius.antal@mindcti.com>
2019-02-12 13:35:27 +0200

    Assignment 5: Practicing Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import {AppComponent} from './app.component';
+import {ActiveUsersComponent} from './active-users/active-users.component';
+import {InactiveUsersComponent} from './inactive-users/inactive-users.component';
+import {UsersService} from './users.service';
+import {CounterService} from './counter.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { InactiveUsersComponent } from './inactive-users/inactive-users.componen
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UsersService, CounterService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/counter.service.ts
@@ -0,0 +1,4 @@
+export class CounterService {
+  activeInactive = 0;
+  inactiveActive = 0;
+}
